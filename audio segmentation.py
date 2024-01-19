8bad257479d5e3365dc4843c514093b0bb949947
QHXRPG <105186795+QHXRPG@users.noreply.github.com>
2024-01-19 17:42:14 +0800

    Update audio segmentation.py

--- audio segmentation.py
+++ audio segmentation.py
@@ -4,6 +4,7 @@ from moviepy.editor import AudioFileClip
 # 指定原始音频文件夹路径和分段后音频文件夹路径
 original_folder_path = r"C:\Users\Administrator\Desktop\sparrow"
 segmented_folder_path = r"C:\Users\Administrator\Desktop\sparrow\k"
+time_interval = 2    # 按照2秒一段进行分段
 
 # 获取文件夹下所有音频文件的路径
 audio_files = [f for f in os.listdir(original_folder_path) if f.endswith('.mp3') or f.endswith('.wav')]
@@ -18,7 +19,7 @@ for audio_file in audio_files:
 
     # 按照2秒一段进行分段
     start_time = 0
-    end_time = 2
+    end_time = time_interval
     segment_number = 1
     while end_time <= total_duration:
         # 获取分段音频
@@ -29,8 +30,8 @@ for audio_file in audio_files:
         segment.write_audiofile(segment_path)
 
         # 更新起始和结束时间
-        start_time += 2
-        end_time += 2
+        start_time += time_interval
+        end_time += time_interval
         segment_number += 1
 
 print("音频文件分段完成")
